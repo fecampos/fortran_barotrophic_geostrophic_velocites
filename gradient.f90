532193bdf34f6dc07cc8bea1de1ac4ccee2b411d
Fernando Campos <32453254+fecampos@users.noreply.github.com>
2021-05-21 13:49:22 -0500

    Update gradient.f90

--- gradient.f90
+++ gradient.f90
@@ -8,33 +8,54 @@
 
       real, intent(out) :: gx(nx,ny), gy(nx,ny)
 
-      real :: dx, dy
+      real :: dx, dy, g(nx,ny)
 
       integer :: i, j
 
-      gx = missing_val
-      gy = missing_val
+      g = f
+
+      where(f.eq.missing_val)
+        g = 0
+      end where
 
       !OMP PARALLEL DO
-      do i = 1,nx-1
+      do i = 2,nx-1
         do j = 1,ny
-          if ((f(i+1,j).ne.missing_val).and.(f(i,j).ne.missing_val)) then
-            call gsw_distance(lon(i+1),lon(i),lat(j),lat(j),dx)
-            gx(i,j) = (f(i+1,j)-f(i,j))/dx
-          end if
+          call gsw_distance(lon(i+1),lon(i-1),lat(j),lat(j),dx)
+          gx(i,j) = (g(i+1,j)-g(i-1,j))/dx
+        end do
+      end do
+      !OMP END PARALLEL DO
+      
+      !OMP PARALLEL DO
+      do j = 1,ny
+        call gsw_distance(lon(2),lon(1),lat(j),lat(j),dx)
+        gx(1,j) = (g(2,j)-g(1,j))/dx
+        call gsw_distance(lon(nx),lon(nx-1),lat(j),lat(j),dx)
+        gx(nx,j) = (g(nx,j)-g(nx-1,j))/dx
+      end do
+      !OMP END PARALLEL DO
+
+      !OMP PARALLEL DO
+      do i = 1,nx
+        do j = 2,ny-1
+          call gsw_distance(lon(i),lon(i),lat(j+1),lat(j-1),dy)
+          gy(i,j) = (g(i,j+1)-g(i,j-1))/dy
         end do
       end do
       !OMP END PARALLEL DO
 
       !OMP PARALLEL DO
       do i = 1,nx
-        do j = 1,ny-1
-          if ((f(i,j+1).ne.missing_val).and.(f(i,j).ne.missing_val)) then
-            call gsw_distance(lon(i),lon(i),lat(j+1),lat(j),dy)
-            gy(i,j) = (f(i,j+1)-f(i,j))/dy
-          end if
-        end do
+        call gsw_distance(lon(i),lon(i),lat(2),lat(1),dy)
+        gy(i,1) = (g(i,2)-g(i,1))/dy
+        call gsw_distance(lon(i),lon(i),lat(ny),lat(ny-1),dy)
+        gy(i,ny) = (g(i,ny)-g(i,ny-1))/dy
       end do
       !OMP END PARALLEL DO
 
-      end subroutine
+      where(f.eq.missing_val)
+        gx = missing_val
+        gy = missing_val
+      end where
+
